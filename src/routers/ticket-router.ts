de2f7337e4f2022a89f61698dcb473cad1f8fb8a
LordAkirast <darkwarish@gmail.com>
2023-11-12 19:07:11 -0300

    tentando fazer funcionar

---
+++ src/routers/ticket-router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { authenticateToken, validateBody } from '@/middlewares';
+import { getEnrollmentByUser, postCreateOrUpdateEnrollment, getAddressFromCEP } from '@/controllers';
+import { createOrUpdateEnrollmentSchema } from '@/schemas';
+
+const ticketsRouter = Router();
+
+ticketsRouter
+  .get('/tickets/types', getAddressFromCEP)
+  .all('/*', authenticateToken)
+  .get('/', getEnrollmentByUser)
+  .post('/', validateBody(createOrUpdateEnrollmentSchema), postCreateOrUpdateEnrollment);
+
+export { ticketsRouter };
